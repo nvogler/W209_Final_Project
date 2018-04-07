839595901b43871666840cedd012213d6974fb59
Nicholas A. Vogler <nvogler@umich.edu>
2018-04-06 22:11:53 -0400

    added tooltips

--- bar.js
+++ bar.js
@@ -48,7 +48,6 @@ d3.csv("/data/feat_imps.csv", function (error, data) {
   		.attr("x",0 - (height / 2) - 20)
 		.text("Importance")
 			.style("font-size", "20px");
-
 	
 	// Define bars
 	g.selectAll(".bar")
@@ -57,58 +56,102 @@ d3.csv("/data/feat_imps.csv", function (error, data) {
 	      .attr("class", "bar")
 	      .attr("x", function(d) { return x(d.attribute); })
 	      .attr("y", function(d) { return y(d.importance); })
-	      .attr("width", 20)
-	      .attr("height", function(d) { return height - y(d.importance); });
+	      .attr("width", 40)
+	      .attr("height", function(d) { return height - y(d.importance); })
+	      .on("mouseover", mouseover)
+	      .on("mouseout", mouseout)
+		  .on("mousemove", function(d) {
+		    var xPosition = d3.mouse(this)[0] - 15;
+		    var yPosition = d3.mouse(this)[1] - 25;
+		    tooltip.attr("transform", "translate(" + xPosition + "," + yPosition + ")");
+		    tooltip.select("text")
+		    	.text(d.attribute + ": " + d.importance);
+		  });
 
+	var tooltip = svg.append("g")
+	  .attr("class", "tooltip")
+	  .style("display", "none");
+
+	tooltip.append("rect")
+	  .attr("width", 30)
+	  .attr("height", 20)
+	  .attr("fill", "white")
+	  .style("opacity", 0.5);
+
+	tooltip.append("text")
+	  .attr("x", 15)
+	  .attr("dy", "1.2em")
+	  .attr("font-size", "12px")
+	  .attr("font-weight", "bold");
+
+	function mouseover() {
+		tooltip.style("display", null);
+	}
+
+	function mouseout() {
+		tooltip.style("display", "none");
+	}
+
+});
 	// Define tooltip
-	var bar = svg.selectAll(".bar")
-		.data(data)
-		.enter().append("g")
-		.attr("class", "g")
-		.attr("transform", function(d) { return "translate(" + "0" + ",0)"; });
+	// var bar = g.selectAll(".bar")
+	// 	.data(data)
+	// 	.enter().append("g")
+	// 	.attr("class", "g")
+	// 	.attr("transform", function(d) { return "translate(" + "0" + ",0)"; });
 
-	bar.selectAll("rect")
-		.data(function(d) {
-			return d.importance;
-		})
-		.enter().append("rect")
-		// Design
-		.attr("class", function(d) {
-        	return "class" + d.attribute.replace(/\s/g, '');
-        })
-		.style("fill", "green")
-		// Dimensions
-		.attr("width", x.rangeBand())
-		.attr("height", function(d) { return y(d.y0) - y(d.y1); })
-		// Positions
-		.attr("y", function(d) { 
-			return y(d.y1);
-		})
-		.attr("x", function(d) { 
-			return x(d.mydate);
-		});
+	// bar.selectAll("rect")
+	// 	.on("mouseover", function(d){
+	// 		console.log("hi");
+			
+	// 		var xPos = parseFloat(d3.select(this).attr("x"));
+	// 		var yPos = parseFloat(d3.select(this).attr("y"));
+
+	// 		g.append("text")
+	// 			.attr("x",xPos)
+	// 			.attr("y",yPos - 10)
+	// 			.attr("class","tooltip")
+	// 			.text(d.attribute +": "+ delta); 
+ //       		});
+	// 	});
+	// bar.selectAll("rect")
+	// 	.data(function(d) {
+	// 		return d.importance;
+	// 	})
+	// 	.enter().append("rect")
+	// 	// Design
+	// 	.attr("class", function(d) {
+ //        	return "class" + d.attribute.replace(/\s/g, '');
+ //        })
+	// 	.style("fill", "green")
+	// 	// Dimensions
+	// 	.attr("width", x.rangeBand())
+	// 	.attr("height", function(d) { return y(d.y0) - y(d.y1); })
+	// 	// Positions
+	// 	.attr("y", function(d) { 
+	// 		return y(d.y1);
+	// 	})
+	// 	.attr("x", function(d) { 
+	// 		return x(d.mydate);
+	// 	});
 
 
-	bar.selectAll("rect")
-       .on("mouseover", function(d){
-       		console.log("hi");
-       		if (active_filter === "0" || d.attribute === active_filter) {
-       			var delta = d.y1 - d.y0;
-				var xPos = parseFloat(d3.select(this).attr("x"));
-				var yPos = parseFloat(d3.select(this).attr("y"));
+	// bar.selectAll("rect")
+ //       .on("mouseover", function(d){
+ //       		console.log("hi");
+ //       		if (active_filter === "0" || d.attribute === active_filter) {
+ //       			var delta = d.y1 - d.y0;
+	// 			var xPos = parseFloat(d3.select(this).attr("x"));
+	// 			var yPos = parseFloat(d3.select(this).attr("y"));
 
-				svg.append("text")
-					.attr("x",xPos)
-					.attr("y",yPos - 10)
-					.attr("class","tooltip")
-					.text(d.attribute +": "+ delta); 
-       		}
-	       })
+	// 			svg.append("text")
+	// 				.attr("x",xPos)
+	// 				.attr("y",yPos - 10)
+	// 				.attr("class","tooltip")
+	// 				.text(d.attribute +": "+ delta); 
+ //       		}
+	//        })
 
-       .on("mouseout",function(){
-          svg.select(".tooltip").remove();                                
-       });
-
-   
-
-})
+ //       .on("mouseout",function(){
+ //          svg.select(".tooltip").remove();                                
+ //       });
